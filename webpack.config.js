5701d39a879d5c3551fe700a204664966c26561e
jeongnaehyeok <cnh0301@gmail.com>
2021-03-09 11:17:21 +0900

    env: webpack Output 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
 const PROJECT_ROOT = path.resolve(__dirname);
+const SRC_PATH = path.resolve(__dirname, 'src');
+const BUILD_PATH = path.resolve(PROJECT_ROOT, 'build');
 
 module.exports = webpackEnv => {
   const mode = webpackEnv.WEBPACK_SERVE ? 'development' : 'production';
@@ -8,5 +10,12 @@ module.exports = webpackEnv => {
   const isEnvProduction = mode === 'production';
   return {
     mode,
+    entry: path.resolve(SRC_PATH, 'index.jsx'),
+    output: {
+      path: BUILD_PATH,
+      filename: isEnvProduction
+        ? 'js/[name].[contenthash:8].js'
+        : 'js/bundle.js',
+    },
   };
 };
