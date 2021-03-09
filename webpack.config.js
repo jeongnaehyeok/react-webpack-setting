cadefb1afd34e1d4556742d58911504c49725806
jeongnaehyeok <cnh0301@gmail.com>
2021-03-09 11:13:28 +0900

    env: webpack Mode 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,4 +2,11 @@ const path = require('path');
 
 const PROJECT_ROOT = path.resolve(__dirname);
 
-module.exports = {};
+module.exports = webpackEnv => {
+  const mode = webpackEnv.WEBPACK_SERVE ? 'development' : 'production';
+  const isEnvDevelopment = mode === 'development';
+  const isEnvProduction = mode === 'production';
+  return {
+    mode,
+  };
+};
